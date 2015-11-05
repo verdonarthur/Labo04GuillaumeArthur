e79af0bc7e565033278312e191da035920d20f5e
gsern1 <guillaume.serneels@gmail.com>
2015-11-05 13:02:13 +0100

    Added files, 1st commit

---
+++ src/Labo_4.java
@@ -0,0 +1,25 @@
+
+/*
+-----------------------------------------------------------------------------------
+Laboratoire : 4
+Fichier     : Labo_4.java
+Auteur(s)   : Guillaume Serneels et Arthur Verdon
+Date        : 03.11.2015
+
+But         : Programme de test de la classe Compte
+
+Remarque(s) : 
+
+Compilateur : jdk1.8.0_60
+-----------------------------------------------------------------------------------
+*/
+
+import compte.*;
+
+public class Labo_4 {
+
+  public static void main(String[] args) {
+     
+  }
+
+}
---
+++ src/compte/Compte.java
@@ -0,0 +1,113 @@
+package compte;
+
+public class Compte {
+
+   private int noCompte;
+
+   private String nomTitulaire;
+
+   private double solde;
+
+   private int debMax;
+
+   private int decMax;
+
+   private static int nbreComptes = 1;
+
+   public Compte(String nomTitulaire) {
+      
+      if(nomTitulaire.isEmpty())
+         throw new IllegalArgumentException();  
+      
+      noCompte = nbreComptes++;
+      nomTitulaire = this.nomTitulaire;
+      solde = 0.0;
+      debMax = 1000;
+      decMax = 800;
+   }
+
+   public Compte(String nomTitulaire, double dépôtInit) {
+      
+      if(nomTitulaire.isEmpty() || dépôtInit < 0)
+         throw new IllegalArgumentException();
+      
+      noCompte = nbreComptes++;
+      nomTitulaire = this.nomTitulaire;
+      solde = dépôtInit;
+      debMax = 1000;
+      decMax = 800;
+   }
+
+   public Compte(String nomTitulaire, double dépôtInit, int decMaxAut) {
+      
+      if(nomTitulaire.isEmpty() || dépôtInit < 0 || decMaxAut < 0)
+         throw new IllegalArgumentException();
+      
+      noCompte = nbreComptes++;
+      nomTitulaire = this.nomTitulaire;
+      solde = dépôtInit;
+      debMax = 1000;
+      decMax = decMaxAut;
+   }
+
+   public Compte(String nomTitulaire, double dépôtInit, int decMaxAut, int debMaxAut) {
+      
+      if(nomTitulaire.isEmpty() || dépôtInit < 0 || decMaxAut < 0 || debMaxAut < 0)
+         throw new IllegalArgumentException();
+      
+      noCompte = nbreComptes++;
+      nomTitulaire = this.nomTitulaire;
+      solde = dépôtInit;
+      debMax = debMaxAut;
+      decMax = decMaxAut;
+   }
+
+   public int getNoCompte() {
+      return noCompte;
+   }
+
+   public String getNomTitulaire() {
+      return "nomTitulaire";
+   }
+
+   public double getSolde() {
+      return solde;
+   }
+
+   public int getDebMax() {
+      return debMax;
+   }
+
+   public int getDecMax() {
+      return decMax;
+   }
+
+   public boolean estADecouvert() {
+      return (solde < 0);
+   }
+
+   public double debAutorisé() {
+      /*
+        
+       
+      if(estADecouvert())
+         return max(decMax + solde, debMax + solde - ;
+      else
+         
+      */
+      return 0.0;
+      //return Math.min(Math.abs(a), b)
+   }
+
+   public void créditer() {
+      // TODO implement here
+   }
+
+   public void débiter() {
+      // TODO implement here
+   }
+
+   public void virement(Compte compteACrediter, double montant) {
+      // TODO implement here
+   }
+}
